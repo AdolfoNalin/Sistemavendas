2bef1e6399b24235b03209acb89513112745d598
AdolfoNalin <nalinadolfo33@gmail.com>
2025-06-29 17:16:49 -0300

    Criando entidade Client

---
+++ SistemaVenda/Model/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Model
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Email { get; set; }
+        public string CEP { get; set; }
+        public string PhoneNumber { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Neighborhood { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string Complement { get; set; }
+    }
+}
