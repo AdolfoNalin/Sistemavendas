4cd0af5fd88cb288301e6d7c33a33b57f4efd224
AdolfoNalin <nalinadolfo33@gmail.com>
2025-07-03 21:19:18 -0300

    Criando ConnectionFactory

---
+++ SistemaVenda/ConnectionFactory/ConnectionFactory.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVenda.Model.Helpers;
+using System.Management;
+using SistemaVenda.br.pro.com.model;
+
+namespace SistemaVenda.br.pro.com.connection
+{
+    public class ConnectionFactory
+    {
+        public static HttpClient ConnectionLocalhost()
+        {
+            string endpoint = SistemaVenda.Model.Helpers.Endpoint.GetEndPoint();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(endpoint);
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", UserResponse.Token);
+            client.Timeout = new TimeSpan(0, 0, 30);
+
+            return client;
+        }
+    }
+}
