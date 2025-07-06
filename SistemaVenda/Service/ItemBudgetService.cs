53612df76d7f62932c29ab47010c8f640619bd7d
AdolfoNalin <nalinadolfo33@gmail.com>
2025-07-06 20:19:44 -0300

    Criando classe itemBudgetService'

---
+++ SistemaVenda/Service/ItemBudgetService.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using SistemaVenda.br.pro.com.connection;
+using SistemaVenda.br.pro.com.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVenda.Service
+{
+    public class ItemBudgetService
+    {
+        #region Get
+        public async Task<List<ItemBudget>> Get(Guid idBudget)
+        {
+            try
+            {
+                List<ItemBudget> list = null;
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.GetAsync($"ItemBudget/{idBudget}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    list = JsonConvert.DeserializeObject<List<ItemBudget>>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show(await response.Content.ReadAsStringAsync());
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+                return null;
+            }
+        }
+        #endregion
+
+        #region Post
+        public async void Post(ItemBudget item)
+        {
+            try
+            {
+                string message = "";
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.PostAsJsonAsync("ItemBudget", item);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+            }
+        }
+        #endregion
+
+        #region Put
+        public async void Put(ItemBudget item)
+        {
+            try
+            {
+                string message = "";
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.PutAsJsonAsync("ItemBudget", item);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+            }
+        }
+        #endregion
+    }
+}
