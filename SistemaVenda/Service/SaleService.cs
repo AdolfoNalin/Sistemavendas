b5701c5dfa6e61fae0b1847abfa7b5eaf9789a5e
AdolfoNalin <nalinadolfo33@gmail.com>
2025-07-07 21:08:13 -0300

    Criando classe SaleService

---
+++ SistemaVenda/Service/SaleService.cs
@@ -0,0 +1,189 @@
+﻿using Newtonsoft.Json;
+using SistemaVenda.br.pro.com.connection;
+using SistemaVenda.br.pro.com.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVenda.Service
+{
+    public class SaleService
+    {
+        #region Get
+        public async Task<List<Sale>> Get()
+        {
+            try
+            {
+                List<ItemReturn> list = null;
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.GetAsync("Sale");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    list = JsonConvert.DeserializeObject<List<ItemReturn>>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show(await response.Content.ReadAsStringAsync(), "Aconteceu um erro");
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+                return null;
+            }
+        }
+        #endregion
+
+        #region GetId
+        public async Task<List<ItemReturn>> Get(Guid id)
+        {
+            try
+            {
+
+                List<ItemReturn> list = null;
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.GetAsync($"Sale/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    list = JsonConvert.DeserializeObject<List<ItemReturn>>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show(await response.Content.ReadAsStringAsync(), "Aconteceu um erro");
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+                return null;
+            }
+        }
+        #endregion
+
+        #region GetSmart
+        public async Task<List<Sale>> Get(string value)
+        {
+            try
+            {
+                List<ItemReturn> list = null;
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.GetAsync($"Sale/{value}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    list = JsonConvert.DeserializeObject<List<ItemReturn>>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show(await response.Content.ReadAsStringAsync(), "Aconteceu um erro");
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+                return null;
+            }
+        }
+        #endregion
+
+        #region Post
+        public async void Post(Sale sale)
+        {
+            try
+            {
+                string message = "";
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.PostAsJsonAsync("Sale", sale);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message += await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    message += await response.Content.ReadAsStringAsync();
+                }
+
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+            }
+        }
+        #endregion
+
+        #region Put
+        public async void Put(Sale sale)
+        {
+            try
+            {
+                string message = "";
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.PutAsJsonAsync("Sale", sale);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message += await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    message += await response.Content.ReadAsStringAsync();
+                }
+
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+            }
+        }
+        #endregion
+
+        #region Delete
+        public async void Delete(Guid id)
+        {
+            try
+            {
+                string message = "";
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.DeleteAsync($"Sale/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message += await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    message += await response.Content.ReadAsStringAsync();
+                }
+
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+            }
+        }
+        #endregion
+    }
+}
