6fca4ee633d6be2589fd8780a8df3a0421153122
AdolfoNalin <nalinadolfo33@gmail.com>
2025-08-15 21:25:26 -0300

    Criando a classe responsavel pela função que gera o hash da senha

---
+++ SistemaVenda/Model/Helpers/PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.br.pro.com.model.Helpers
+{
+    public static class PasswordGenerator
+    {
+        #region PasswordGenerator
+        public static string Generator(string password)
+        {
+            string hashPassword;
+            using(SHA256 pass = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                byte[] result = pass.ComputeHash(bytes);
+                hashPassword = Encoding.UTF8.GetString(result);
+            }
+
+            return hashPassword;
+        }
+        #endregion
+    }
+}
