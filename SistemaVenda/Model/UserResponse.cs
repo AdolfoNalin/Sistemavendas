4f50247a1a1ad6fb65b06ce1a3d1932b1b5f271a
AdolfoNalin <nalinadolfo33@gmail.com>
2025-08-15 21:29:25 -0300

    Criadas para mandar e receber requisições

--- SistemaVenda/Model/UserResponse.cs
+++ SistemaVenda/Model/UserResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaVenda.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -9,11 +10,7 @@ namespace SistemaVenda.br.pro.com.model
 {
     public class UserResponse
     {
-        public static Guid Id { get; set; }
-        public static string Name { get; set; }
-        public static string Password { get; set; }
-        public static string Login { get; set; }
-        public static string Function { get; set; }
-        public static string Token { get; set; }
+        public User User { get; set; }
+        public string Token { get; set; }
     }
 }
