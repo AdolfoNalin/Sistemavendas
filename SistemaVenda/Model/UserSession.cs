29eb5a9c224b4a4ded20d8f73c5b429b8ae2363c
AdolfoNalin <nalinadolfo33@gmail.com>
2025-08-17 18:45:52 -0300

    Criando classe
    Classe estatica, responsável por por conter os dados de acesso do usuário logado

---
+++ SistemaVenda/Model/UserSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SistemaVenda.Model
+{
+    public class UserSession
+    {
+        public static Guid Id { get; set; }
+        public static string Login  { get; set; }
+        public static string Name { get; set; }
+        public static string Password { get; set; }
+        public static string Function { get; set; }
+        public static string Token { get; set; }
+    }
+}
