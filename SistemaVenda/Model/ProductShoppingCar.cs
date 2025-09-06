7f8a1a67e3ed026ac66238aef231dac813509580
AdolfoNalin <nalinadolfo33@gmail.com>
2025-09-06 20:17:07 -0300

    Create the class for model of dgShoppingCar

---
+++ SistemaVenda/Model/ProductShoppingCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Model
+{
+    public class ProductShoppingCar
+    {
+        #region Campos
+        /// <summary>
+        /// Campos da classe Produtos
+        /// </summary>
+        public string ShortDescription { get; set; }
+        public decimal CashPrice { get; set; }
+        public decimal TermPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public double Amount { get; set; }
+        #endregion
+    }
+}
