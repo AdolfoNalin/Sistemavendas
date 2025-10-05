669a08bbd8ff70baa548baed985f3fc67828a566
AdolfoNalin <nalinadolfo33@gmail.com>
2025-10-05 14:03:56 -0300

    Criando o objeto Return
    Classe dedicada aos a tributos de Devolução

---
+++ SistemaVenda/Model/Return.cs
@@ -0,0 +1,13 @@
+﻿using SistemaVenda.br.pro.com.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Model
+{
+    public class Return : Sale
+    {
+    }
+}
