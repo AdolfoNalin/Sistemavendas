a32d26f16ac5e9ae47967a38c58fa9c137f162af
AdolfoNalin <nalinadolfo33@gmail.com>
2025-10-13 20:53:25 -0300

    Create the Class CashDesckService

---
+++ SistemaVenda/Service/CashDesckService.cs
@@ -0,0 +1,192 @@
+﻿using iTextSharp.text.io;
+using Newtonsoft.Json;
+using SistemaVenda.br.pro.com.connection;
+using SistemaVenda.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.NetworkInformation;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVenda.Service
+{
+    public class CashDesckService
+    {
+        #region Get
+        public async Task<BindingList<CashDesck>> Get()
+        {
+            try
+            {
+                BindingList<CashDesck> cashDescks = new BindingList<CashDesck>();
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.GetAsync("CashDesck");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    cashDescks = JsonConvert.DeserializeObject<BindingList<CashDesck>>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show(await response.Content.ReadAsStringAsync());
+                }
+
+                return cashDescks;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+                return null;
+            }
+        }
+        #endregion
+
+        #region GetDate
+        public async Task<BindingList<CashDesck>> Get(DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                BindingList<CashDesck> cashDescks = new BindingList<CashDesck>();
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.GetAsync($"CashDesck/Date?startDate={startDate}&endDate={endDate}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    cashDescks = JsonConvert.DeserializeObject<BindingList<CashDesck>>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show(await response.Content.ReadAsStringAsync());
+                }
+
+                return cashDescks;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+                return null;
+            }
+        }
+        #endregion
+
+        #region GetEmployee
+        public async Task<BindingList<CashDesck>> GetEmployee(string value)
+        {
+            try
+            {
+                BindingList<CashDesck> cashDescks = new BindingList<CashDesck>();
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.GetAsync($"CashDesck/Employee/{value}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    cashDescks = JsonConvert.DeserializeObject<BindingList<CashDesck>>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show(await response.Content.ReadAsStringAsync());
+                }
+
+                return cashDescks;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+                return null;
+            }
+        }
+        #endregion
+
+        #region Post
+        public async void Post(CashDesck cash)
+        {
+            try
+            {
+                string message = "";
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.PostAsJsonAsync("CashDesck", cash);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message = await response.Content.ReadAsStringAsync();    
+                }
+                else
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+            }
+        }
+        #endregion
+
+        #region Put
+        public async void Put(CashDesck cash)
+        {
+            try
+            {
+                string message = "";
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.PutAsJsonAsync("CashDesck", cash);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+            }
+        }
+        #endregion
+
+        #region Delete
+        public async void Delete(Guid id)
+        {
+            try
+            {
+                string message = "";
+
+                HttpClient client = ConnectionFactory.ConnectionLocalhost();
+                HttpResponseMessage response = await client.DeleteAsync($"CashDesck/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}");
+            }
+        }
+        #endregion
+    }
+}
