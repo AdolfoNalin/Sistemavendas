72da414e2fa6d1b2c897f0effabe8bcac0cdcd8a
AdolfoNalin <nalinadolfo33@gmail.com>
2025-11-12 20:43:40 -0300

    Criando Função responsável por verificar a senha passada

---
+++ SistemaVenda/Model/Helpers/VerificationPassword.cs
@@ -0,0 +1,35 @@
+﻿using SistemaVenda.br.pro.com.model.Helpers;
+using SistemaVenda.br.pro.com.model;
+using SistemaVenda.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVenda.Model.Helpers
+{
+    public class VerificationPassword
+    {
+        public static async Task<UserResponse> Verification(string password) 
+        {
+
+            string hash = PasswordGenerator.Generator(password);
+
+            UserResponse response = await UserService.GetPassword(hash);
+
+            if (response.User != null)
+            {
+                UserSession.Id = response.User.Id;
+                UserSession.EmployeeId = response.User.EmployeeId;
+                UserSession.Name = response.User.Name;
+                UserSession.Login = response.User.Login;
+                UserSession.Password = response.User.Password;
+                UserSession.Token = response.Token;
+            }
+
+            return response;
+        }
+    }
+}
