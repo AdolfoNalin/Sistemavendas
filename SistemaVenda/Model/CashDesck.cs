aa6be2bf907283bb836f0c8c991a2101a66942e5
AdolfoNalin <nalinadolfo33@gmail.com>
2025-11-12 20:36:42 -0300

    Criando Classe statica para armazenamento de objeto provisorio

---
+++ SistemaVenda/Model/CashDesck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Model
+{
+    public class CashDesck
+    {
+        public static Guid Id { get; set; }
+        public static Guid UserId { get; set; }
+        public static DateTime Date { get; set; }
+        public static decimal OpeningAmount { get; set; }
+        public static Enable Enable { get; set; }
+        public static IsCashSession Status { get; set; }
+        public static decimal Total { get; set; }
+    }
+}
