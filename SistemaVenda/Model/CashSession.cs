29162c0885ed1a364dd21446f1a479fa19fce87d
AdolfoNalin <nalinadolfo33@gmail.com>
2025-11-12 20:39:54 -0300

    Criando classe caixa de acesso

---
+++ SistemaVenda/Model/CashSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Model
+{
+    public enum Enable
+    {
+        Enable,
+        Disabel
+    }
+
+    public enum IsCashSession
+    {
+        Close,
+        Open
+    }
+    public class CashSession
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal OpeningAmount{ get; set; }
+        public Enable Enable { get; set; }
+        public IsCashSession Status { get; set; }
+        public decimal Total { get; set; }
+    }
+}
