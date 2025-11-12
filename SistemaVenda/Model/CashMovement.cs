a19703672bcd701e2b054bbbeccf20d555b08250
AdolfoNalin <nalinadolfo33@gmail.com>
2025-11-12 20:38:47 -0300

    Craindo classe de movimentação de caixa

---
+++ SistemaVenda/Model/CashMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Model
+{
+    public enum Type
+    {
+        Entry,
+        Exit
+    }
+
+    public class CashMovement
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CashSessionId { get; set; }
+        public decimal Amount { get; set; }
+        public Type Type { get; set; } 
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
