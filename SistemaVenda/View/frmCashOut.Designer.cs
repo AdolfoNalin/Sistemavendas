d4a30f8327d4d3c206febe6f9b501b6b65fb481e
AdolfoNalin <nalinadolfo33@gmail.com>
2025-12-05 11:47:33 -0300

    Create and programming the screen frmCashOut

---
+++ SistemaVenda/View/frmCashOut.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SistemaVenda.View
+{
+    partial class frmCashOut
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblValueOut = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(323, 185);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(102, 34);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Avançar";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(46, 185);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(102, 34);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(95, 31);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(100, 20);
+            this.txtType.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(95, 86);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(288, 53);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(62, 35);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(31, 13);
+            this.lblType.TabIndex = 4;
+            this.lblType.Text = "Tipo:";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(34, 89);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(58, 13);
+            this.lblDescription.TabIndex = 5;
+            this.lblDescription.Text = "Descrição:";
+            // 
+            // lblValueOut
+            // 
+            this.lblValueOut.AutoSize = true;
+            this.lblValueOut.Location = new System.Drawing.Point(205, 35);
+            this.lblValueOut.Name = "lblValueOut";
+            this.lblValueOut.Size = new System.Drawing.Size(77, 13);
+            this.lblValueOut.TabIndex = 7;
+            this.lblValueOut.Text = "Valor de saida:";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(283, 31);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtAmount.TabIndex = 6;
+            // 
+            // frmCashOut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 241);
+            this.Controls.Add(this.lblValueOut);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnNext);
+            this.Name = "frmCashOut";
+            this.Text = "frmCashOut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblValueOut;
+        public System.Windows.Forms.TextBox txtType;
+        public System.Windows.Forms.TextBox txtDescription;
+        public System.Windows.Forms.TextBox txtAmount;
+    }
+}
---
+++ SistemaVenda/View/frmCashOut.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVenda.View
+{
+    public partial class frmCashOut : Form
+    {
+        public frmCashOut()
+        {
+            InitializeComponent();
+        }
+
+        #region btnNext_Click
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        #endregion
+
+        #region btnCancel_Click
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
