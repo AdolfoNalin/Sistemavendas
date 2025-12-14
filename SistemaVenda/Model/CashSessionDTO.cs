b1e39dadfd0c5a95feffa0653047dc249b177bd0
AdolfoNalin <nalinadolfo33@gmail.com>
2025-12-14 05:28:19 -0300

    Criando class modelo para exibição de dados no data grid

---
+++ SistemaVenda/Model/CashSessionDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Model
+{
+    public class CashSessionDTO
+    {
+        public Guid Id { get; set; }
+        public string UserName{ get; set; }
+        public DateTime Date { get; set; }
+        public decimal OpeningAmount { get; set; }
+        public Enable Enable { get; set; }
+        public IsCashSession Status { get; set; }
+        public decimal Total { get; set; }
+    }
+}
